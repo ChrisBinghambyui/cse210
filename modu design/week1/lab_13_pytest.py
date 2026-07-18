bb5d21add5fd3a530a170f4cc157c84103beaf3e
ChrisBinghambyui <bin23003@byui.edu>
2026-07-18 14:46:49 +0000

    Jul 18, 2026, 8:46 AM

---
+++ modu design/week1/lab_13_pytest.py
@@ -0,0 +1,12 @@
+import pytest
+from lab13 import run_sort
+
+def test_quicksort():
+    assert run_sort([47,12,83,5,61,29,74,38,90,16,55,42,7,68,31,99,23,50,14,77]) == [5,7,12,14,16,23,29,31,38,42,47,50,55,61,68,74,77,83,90,99]
+    assert run_sort([1,2,3,4,5]) == [1,2,3,4,5]
+    assert run_sort([5,4,3,2,1]) == [1,2,3,4,5]
+    assert run_sort([7,7,7,7,7]) == [7,7,7,7,7]
+    assert run_sort([42]) == [42]
+    assert run_sort([9,1]) == [1,9]
+    assert run_sort([-3,-1,-7,-2,-5]) == [-7,-5,-3,-2,-1]
+    assert run_sort([3,-2,7,-5,0]) == [-5,-2,0,3,7]
